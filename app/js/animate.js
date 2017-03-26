dd1530f83f9f1661985460b3f2dd7d661110c21a
chenyixin <onepiececee@163.com>
2017-03-26 20:32:18 +0800

    cv
    cv

--- app/js/animate.js
+++ app/js/animate.js
@@ -2,7 +2,12 @@
 
 var t = new TimelineMax();
 $('#menu').click(function(){
+
     t.staggerTo('.nav-li',0.2,{opacity:1},-0.5);
+});
+
+$('#close').click(function(){
+    t.staggerTo('.nav-li',0.1,{opacity:0},0);
 });
 
 })();
--- app/js/weixin.js
+++ app/js/weixin.js
@@ -1,17 +1,17 @@
 ;(function(){
 
 $('#weixin').click(function(){
-    $('#float02').show();
+    $('#float02').fadeIn(1000).show();
 });
 
 
 $('#close02').click(function(){
-    $('#float02').hide();
+    $('#float02').fadeOut().hide();
 });
 
 $('#nav-li-weixin').click(function(){
-    $('#float').hide();
-    $('#float02').show();
+    $('#float').fadeOut(1000).hide();
+    $('#float02').fadeIn(1000).show();
 });
 
 })();
